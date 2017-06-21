9bd3e331f39b7550671b468857ccb9b621b96d17
Brett Merrill <bmerril2@uoregon.edu>
2017-06-20 19:16:58 -0700

    Update Main.java

--- Java_GUI_Paint_Project/Main.java
+++ Java_GUI_Paint_Project/Main.java
@@ -7,10 +7,18 @@ an interactive paint application with a mix of standard and custom
 wigits. This invloved opening a UI window to display the various painting 
 tools. 
 
-The application stores data in the file PaintFrame.java by capturing
+The application stores data in the class PaintFrame by capturing
 and drawing points when the user clicks or drags their mouse. Each paint 
-point is stored in an ArrayList of instances of the PaintPoint class using
+point is stored in an ArrayList of instances of the PaintPoint class.
+Using my implementation of the Jpanel paint component I was able to
+point each instance in the list to the canvas. 
 
+My application contains 3  buttons with different color choices and 1 for
+custom color choice. These colors last until a new color is selected. 
+I added 2 buttons for Shape choice. Circle of Square. 
+I also added 3 buttons to select different sizes of paint brush. 
+Lastly I added a clear button to reset the application to a blank canvas. 
+*/
 //Instantiates the paint frame
 import javax.swing.JFrame;
 
